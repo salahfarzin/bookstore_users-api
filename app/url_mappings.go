18ba08d2dc77d7eb4bc9da2b75cf1afed718b162
Salah <ahoraian@gmail.com>
2022-08-21 11:50:49 +0000

    initial commit

---
+++ app/url_mappings.go
@@ -0,0 +1,18 @@
+package app
+
+import (
+	"github.com/salahfarzin/bookstore_users-api/controllers/ping"
+	"github.com/salahfarzin/bookstore_users-api/controllers/users"
+)
+
+func mapUrls() {
+	router.GET("/ping", ping.Ping)
+s
+	router.POST("/users", users.Create)
+	router.GET("/users/:user_id", users.Get)
+	router.PUT("/users/:user_id", users.Update)
+	router.PATCH("/users/:user_id", users.Update)
+	router.DELETE("/users/:user_id", users.Delete)
+	router.GET("/internal/users/search", users.Search)
+	router.POST("/users/login", users.Login)
+}
---
+++ main.go
@@ -0,0 +1,9 @@
+package main
+
+import (
+	"github.com/salahfarzin/bookstore_users-api/app"
+)
+
+func main() {
+	app.StartApplication()
+}
