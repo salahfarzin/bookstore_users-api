7fa93d576676fa5d6b00f5f8a60e409694442d64
Salah <ahoraian@gmail.com>
2022-08-21 11:52:04 +0000

    fix dot bug

--- app/url_mappings.go
+++ app/url_mappings.go
@@ -7,7 +7,7 @@ import (
 
 func mapUrls() {
 	router.GET("/ping", ping.Ping)
-s
+
 	router.POST("/users", users.Create)
 	router.GET("/users/:user_id", users.Get)
 	router.PUT("/users/:user_id", users.Update)
